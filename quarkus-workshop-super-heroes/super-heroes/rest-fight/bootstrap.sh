0100881d343accdc89b79edd1bc3d037bd94f8f0
David Cavanaugh <dcavanau@us.ibm.com>
2021-03-29 18:03:30 -0400

    updates to rest-fight

--- quarkus-workshop-super-heroes/super-heroes/rest-fight/bootstrap.sh
+++ quarkus-workshop-super-heroes/super-heroes/rest-fight/bootstrap.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 # tag::adocSnippet[]
-mvn io.quarkus:quarkus-maven-plugin:1.9.2.Final:create \
+cd quarkus-workshop-super-heroes/super-heroes
+mvn io.quarkus:quarkus-maven-plugin:1.13.0.Final:create \
     -DprojectGroupId=io.quarkus.workshop.super-heroes \
     -DprojectArtifactId=rest-fight \
     -DclassName="io.quarkus.workshop.superheroes.fight.FightResource" \
     -Dpath="api/fights"
 cd rest-fight
-./mvnw quarkus:add-extension -Dextensions="jdbc-postgresql,hibernate-orm-panache,hibernate-validator,quarkus-resteasy-jsonb,smallrye-openapi,kafka"
+./mvnw quarkus:add-extension -Dextensions="jdbc-postgresql,hibernate-orm-panache,hibernate-validator,quarkus-resteasy-jsonb,quarkus-smallrye-openapi,quarkus-smallrye-metrics,quarkus-smallrye-health,quarkus-smallrye-reactive-messaging-kafka,smallrye-fault-tolerance,com.fasterxml.jackson.core:jackson-databind,org.testcontainers:junit-jupiter:1.15.2,org.testcontainers:postgresql:1.15.2,com.fasterxml.jackson.datatype:jackson-datatype-jsr310,org.testcontainers:kafka,org.scala-lang:scala-library"
 # end::adocSnippet[]
